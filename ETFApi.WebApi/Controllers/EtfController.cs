71459310a2670fc4b3d795ba9001918f6a13210f
Nicolò Belluco <nicolobelluco98@gmail.com>
2021-03-14 14:17:25 +0100

    Created ETFController.cs

---
+++ ETFApi.WebApi/Controllers/EtfController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETFApi.WebApi.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class EtfController : ControllerBase
+    {
+        
+    }
+}
