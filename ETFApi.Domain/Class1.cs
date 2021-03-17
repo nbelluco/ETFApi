1293072844af01c67f6ad6f0cc15a83edb0c8b56
Nicolò Belluco <nicolobelluco98@gmail.com>
2021-03-17 21:40:06 +0100

    Added class BaseEntity

--- ETFApi.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ETFApi.Domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ ETFApi.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,7 @@
+namespace ETFApi.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
