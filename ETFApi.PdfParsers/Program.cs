43dff9002e9b399c1cf0e0f27e34000db340d622
Nicolò Belluco <nicolobelluco98@gmail.com>
2021-03-21 14:37:11 +0100

    Created ETFApi.PdfParsers project inside solution

---
+++ ETFApi.PdfParsers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ETFApi.PdfParsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
