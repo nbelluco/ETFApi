9bcc32560f687c2f143bd0607429bcce7a676e90
Nicolò Belluco <nicolobelluco98@gmail.com>
2021-03-21 22:10:04 +0100

    Added methods to extract "Performance" content from substrings

--- ETFApi.PdfParsers/Parsers/iSharesPdfParser.cs
+++ ETFApi.PdfParsers/Parsers/iSharesPdfParser.cs
@@ -22,7 +22,91 @@ namespace ETFApi.PdfParsers.Parsers
 
         private protected override Etf ParseContentToEtfObject()
         {
-            throw new NotImplementedException();
+            return new Etf
+            {
+                FundPerformances = ExtractPerformanceValues()
+            };
+        }
+
+        private List<FundPerformanceValue> ExtractPerformanceValues()
+        {
+            var performances = new List<FundPerformanceValue>();
+            
+            var performanceSpan = ContentPairs["Performance"].AsSpan();
+            
+            var fundIndex = performanceSpan.IndexOf("Fund");
+            var benchmarkIndex = performanceSpan.IndexOf("Benchmark");
+
+            var fundSpan = performanceSpan
+                .Slice(fundIndex + 4, benchmarkIndex - fundIndex - 4);
+            
+            var benchmarkSpan = performanceSpan
+                .Slice(benchmarkIndex + 9);
+
+            var periodsSpan = performanceSpan.Slice(0, fundIndex);
+
+            var periods = new List<string>
+            {
+                periodsSpan.Slice(0, 23).ToString(),
+                periodsSpan.Slice(24, 23).ToString(),
+                periodsSpan.Slice(47, 23).ToString(),
+                periodsSpan.Slice(71, 23).ToString(),
+                periodsSpan.Slice(95, 23).ToString(),
+                periodsSpan.Slice(109).ToString(),
+            };
+
+            var fundPerformances = ExtractPerformanceRow(fundSpan);
+            var benchmarkPerformances = ExtractPerformanceRow(benchmarkSpan);
+            
+            for (int i = 0; i < periods.Count; i++)
+            {
+                performances.Add(new FundPerformanceValue
+                {
+                    Period = periods[i],
+                    Performance = fundPerformances[i],
+                    BenchmarkPerformance = benchmarkPerformances[i]
+                });
+            }
+
+            return performances;
+        }
+
+        private List<string> ExtractAnnualisedPerformanceValues()
+        {
+            var performances = new List<string>();
+            return performances;
+        }
+
+        private List<string> ExtractPerformanceRow(ReadOnlySpan<char> span)
+        {
+            var charIndex = 0;
+            var lastFoundIndex = 0;
+            
+            var performanceRow = new List<string>();
+            
+            while (charIndex < span.Length)
+            {
+                switch (span[charIndex])
+                {
+                    case 'N':
+                        performanceRow.Add("N/A");
+                        lastFoundIndex = charIndex += 3;
+                        charIndex += 3;
+                        break;
+                    case '%':
+                        performanceRow.Add(
+                            span
+                                .Slice(lastFoundIndex, charIndex - lastFoundIndex)
+                                .ToString()
+                        );
+                        break;
+                    default:
+                        charIndex += 1;
+                        break;
+                }
+            }
+
+            return performanceRow;
         }
     }
 }
