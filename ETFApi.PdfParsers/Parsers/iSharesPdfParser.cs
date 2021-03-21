e3cfc9313b0a5f470012c15f38e27226f6a564ed
Nicolò Belluco <nicolobelluco98@gmail.com>
2021-03-21 22:09:04 +0100

    Added list of KeyValuePairs to define content paragraphs of iShares pdf

--- ETFApi.PdfParsers/Parsers/iSharesPdfParser.cs
+++ ETFApi.PdfParsers/Parsers/iSharesPdfParser.cs
@@ -12,7 +12,12 @@ namespace ETFApi.PdfParsers.Parsers
 
         protected override List<KeyValuePair<string, (string, string)>> ContentParts { get; set; } = new()
         {
-            new KeyValuePair<string, (string, string)>("", ("", ""))
+            new KeyValuePair<string, (string, string)>("Performance", ("12 MONTH PERFORMANCE PERIODS (% EUR)", "ANNUALISED PERFORMANCE (% EUR)")),
+            new KeyValuePair<string, (string, string)>("AnnualisedPerformance", ("ANNUALISED PERFORMANCE (% EUR)", "The figures shown")),
+            new KeyValuePair<string, (string, string)>("KeyFacts", ("KEY FACTS", "TOP HOLDINGS (%)")),
+            new KeyValuePair<string, (string, string)>("TopHoldings", ("TOP HOLDINGS (%)", "Holdings are subject to change")),
+            new KeyValuePair<string, (string, string)>("PortfolioCharacteristics", ("PORTFOLIO CHARACTERISTICS", "DEALING INFORMATION")),
+            new KeyValuePair<string, (string, string)>("DealingInformation", ("DEALING INFORMATION", "GEOGRAPHIC BREAKDOWN")),
         };
 
         private protected override Etf ParseContentToEtfObject()
